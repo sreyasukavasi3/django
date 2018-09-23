b3896f08ccede5a78a96418c39505e11f80e4496
Sreya <33253396+sreyasukavasi3@users.noreply.github.com>
2018-09-23 23:59:55 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,6 @@
-
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^music/', include('music.urls')),
-]
+urlpatterns = (
+    url(r'^$', views.index, name='index'),
+)
